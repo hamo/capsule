6a5f21df7a17b8705aff6cb92c177d6263a9f250
Yang Bai <hamo.by@gmail.com>
2015-03-23 18:53:55 +0800

    commands: start of docker-build. Need to finish docker-pull first

--- capsule/commands/docker.go
+++ capsule/commands/docker.go
@@ -1,6 +1,12 @@
 package commands
 
-import ()
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/docker/docker/builder/parser"
+)
 
 var (
 	dockerBuildCommand = CapsuleCommand{
@@ -20,7 +26,44 @@ func init() {
 }
 
 func cmdDockerBuild(args []string, cmdEnv *CommandEnv) error {
+	dockerBuildFlag := flag.NewFlagSet("docker build command", flag.ExitOnError)
+
+	dockerBuildFlag.Usage = func() {
+		fmt.Fprintf(os.Stderr,
+			"Usage: capsule docker-build [options] PATH\n\n")
+		dockerBuildFlag.PrintDefaults()
+	}
+
+	var (
+		flDockerImageName string
+	)
+
+	dockerBuildFlag.StringVar(&flDockerImageName, "name", "", "docker image name")
+
+	dockerBuildFlag.Parse(args)
+
+	if dockerBuildFlag.NArg() != 1 {
+		dockerBuildFlag.Usage()
+		return nil
+	}
+
+	dockerFileURI := dockerBuildFlag.Arg(0)
+	// FIXME: support URL
+	if fi, err := os.Stat(dockerFileURI); err != nil || !fi.Mode().IsRegular() {
+		cmdEnv.Logger.Fatalf("can not read file %s", dockerFileURI)
+	}
+
+	f, err := os.Open(dockerFileURI)
+	if err != nil {
+		cmdEnv.Logger.Fatalf("open file %s failed: %s", dockerFileURI, err)
+	}
+
+	ast, err := parser.Parse(f)
+
+	cmdEnv.Logger.Fatalf("%+v", ast.Dump())
+
 	return nil
+
 }
 
 func cmdDockerPull(args []string, cmdEnv *CommandEnv) error {
